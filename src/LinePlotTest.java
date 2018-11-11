ef0067e7c97f12b301c76c8c8b6de2d7002042bd
ShirKiBakal <shiranbakal@gmail.com>
2018-11-11 19:55:36 +0200

    Added Gral!

---
+++ src/LinePlotTest.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+
+import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.plots.XYPlot;
+import de.erichseifert.gral.ui.InteractivePanel;
+
+public class LinePlotTest extends JFrame  {
+	
+	   public LinePlotTest() {
+	        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	        setSize(800, 600);
+	        Polynom poly = new Polynom("1x^3");
+	        // Insert rest of the code here
+	        DataTable data = new DataTable(Double.class, Double.class);
+	        for (double x = -5.0; x <= 5.0; x+=0.25) {
+	            double y = poly.f(x);
+	            data.add(x, y);
+	        }
+	        XYPlot plot = new XYPlot(data);
+	        getContentPane().add(new InteractivePanel(plot));
+	    }
+	   
+	   
+	    public static void main(String[] args) {
+	        LinePlotTest frame = new LinePlotTest();
+	        frame.setVisible(true);
+
+	    }
+	}
